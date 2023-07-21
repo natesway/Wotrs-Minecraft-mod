7f7faf562c4f67ec1031122f43f7f2e02e63e52a
BuildTools <unconfigured@null.spigotmc.org>
2023-07-20 21:28:59 -0400

    Upload Code

---
+++ src/client/kotlin/com/shatterstar9652/wotrs/ExampleModClient.kt
@@ -0,0 +1,9 @@
+package com.shatterstar9652.wotrs
+
+import net.fabricmc.api.ClientModInitializer
+
+object ExampleModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
